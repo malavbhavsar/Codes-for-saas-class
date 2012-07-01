76c1d52a5a6e42671904ab97be720c3b2f9b27fa
Malav Bhavsar <malav.bhavsar@gmail.com>
2012-07-01 09:00:27 -0700

    All specs green. Now using movie ID to search the movie. still have to do sad path

--- spec/controllers/movie_controller_spec.rb
+++ spec/controllers/movie_controller_spec.rb
@@ -5,17 +5,17 @@ describe MoviesController do
     before :each do
       @m = double('movie')
       @m2 = double('movie')
-      @m.stub(:title=>'Star Wars',:director=>'George Lucas')
-      @m2.stub(:title=>'THX-1138',:director=>'George Lucas')
+      @m.stub(:id=>1,:title=>'Star Wars',:director=>'George Lucas')
+      @m2.stub(:id=>2,:title=>'THX-1138',:director=>'George Lucas')
     end
     it 'should call the model method that performs THE search' do
-      Movie.should_receive(:find_movie_with_same_director).with(@m.director).and_return([@m,@m2])
-      post :find_movie_with_same_director,{:mov => @m.director}
+      Movie.should_receive(:find_movie_with_same_director).with(@m.id.to_s).and_return([@m,@m2])
+      post :find_movie_with_same_director,{:mov => @m.id}
     end
     describe 'after valid search' do
       before :each do
         Movie.stub(:find_movie_with_same_director).and_return([@m,@m2])
-        post :find_movie_with_same_director,{:mov => @m.director}
+        post :find_movie_with_same_director,{:mov => @m.id}
       end
       it 'should select the Search Results template for rendering' do
         response.should render_template('find_movie_with_same_director')
